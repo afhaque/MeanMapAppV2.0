e4f3667c3173967b21d34ccf39203d5376ba6090
Ahmed Haque <afhaque@rice.edu>
2015-09-19 11:32:22 -0500

    Fixed the Long/Lat coordinator in Angular.

--- public/js/core.js
+++ public/js/core.js
@@ -1,22 +1,27 @@
-/**
- * Created by Ahmed on 9/18/2015.
- */
+angular.module('meanMapApp',['geolocation'])
+    .controller('mainCtrl', function ($scope, $http, geolocation) {
 
-var meanMapApp = angular.module('meanMapApp', []);
+        // Initialize Variables
+        $scope.formData = {};
+        var coords = {};
 
-function mainController($scope, $http) {
-    $scope.formData = {};
+        // Get Location on Window Load
+        geolocation.getLocation().then(function(data){
+            coords = {lat:data.coords.latitude, long:data.coords.longitude};
+            $scope.formData.location  = parseFloat(coords.lat).toFixed(3) + "," + parseFloat(coords.long).toFixed(3);
+        });
 
-    // when submitting the form, send the text to the api
-    $scope.createUser = function(){
-        $http.post('/api/users', $scope.formData)
-            .success(function(data){
-                $scope.formData = {}; // clear the form so user can start a fresh
-                $scope.users = data;
-                console.log(data);
-            })
-            .error(function(data){
-                console.log('Error: ' + data);
-            });
-    };
-}
+        // Post New User
+        $scope.createUser = function(){
+            $http.post('/api/users', $scope.formData)
+                .success(function(data){
+                    $scope.formData = {}; // clear the form so user can start a fresh
+                    $scope.users = data;
+                    console.log(data);
+                })
+                .error(function(data){
+                    console.log('Error: ' + data);
+                });
+        };
+
+    });
