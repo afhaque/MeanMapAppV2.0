9c4c9a2c36d71800529634c0f444800cf76f703f
Ahmed Haque <afhaque@rice.edu>
2015-09-21 20:44:16 -0500

    Enlarged the map app a bit for bigger displays.

--- app/config.js
+++ app/config.js
@@ -11,6 +11,13 @@ module.exports = {
         port: 27017
     },
 
+    modulus:
+    {
+        name: "scotch-user-map-ec2",
+        url: "mongodb://sensodx:jmcdevitt@ec2-52-2-231-180.compute-1.amazonaws.com/sensodx-api-prod",
+        port: 27017
+    },
+
     local:
     {
         name: "scotch-user-map-local",
