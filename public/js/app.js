d4ed446c98f99775b18e541f5c2d89ab7c190e69
afhaque <afhaque@rice.edu>
2015-09-27 13:58:01 -0500

    Incorporated ngRoute and Angular Routing

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,10 @@
-var app = angular.module('meanMapApp', ['addCtrl', 'queryCtrl', 'geolocation', 'gservice']);
+var app = angular.module('meanMapApp', ['addCtrl', 'queryCtrl', 'geolocation', 'gservice', 'ngRoute'])
+    .config(function($routeProvider){
+        $routeProvider.when('/join', {
+            controller: 'addCtrl',
+            templateUrl: 'partials/addForm.html',
+        }).when('/find', {
+            controller: 'queryCtrl',
+            templateUrl: 'partials/queryForm.html',
+        });
+    });
